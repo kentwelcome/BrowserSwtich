dcf0352e7328b7e98195d6b3fd4a24bdfda8d353
Kent Huang <kentwelcome@gmail.com>
2015-05-16 02:56:04 +0800

    Initial Commit

---
+++ BrowserSwitch/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  BrowserSwitch
+//
+//  Created by Kent Huang on 2015/5/16.
+//  Copyright (c) 2015年 Kent Huang. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
